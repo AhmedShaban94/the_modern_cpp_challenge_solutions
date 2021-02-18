46298a511521f0c9455a660f284e4cc76c8e242c
Ahmed_Shaban <AhmedShaban94>
2021-02-18 20:52:04 +0400

    updated cmakelists file.

---
+++ chapter01/problem_04/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// chapter-1 -> problem4 (largest prime smaller than given number)
+int main()
+{
+    unsigned int limit{ 0 }, prime{ 0 };
+    std::cout << "Enter the number\n";
+    std::cin >> limit;
+
+    for (int i = 2; i < limit; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+            if (j % i == 0)
+                prime = i;
+    }
+    std::cout << "largest prime number before the given limit " << limit
+              << " is :" << prime << '\n';
+    return EXIT_SUCCESS;
+}
