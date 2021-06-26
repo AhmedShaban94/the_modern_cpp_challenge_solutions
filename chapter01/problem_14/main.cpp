e1d27bf8a38f0a2060c2a15232b29e28ea7ba2ae
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2021-06-26 17:43:56 +0400

    implementation of chapter01/problem_14

--- chapter01/problem_14/main.cpp
+++ chapter01/problem_14/main.cpp
@@ -5,7 +5,30 @@
 #include <iterator>
 #include <vector>
 
-int main()
+bool validate_ISBN(const std::string_view str)
 {
-    return EXIT_SUCCESS; 
+    std::size_t total{0};
+    for (std::size_t i = 10; i >= 2; --i)
+    {
+        const auto num = str.at(10 - i) - '0';
+        total += num * i;
+    }
+    str.back() == 'X' ? total += 10 : total += str.back() - '0';
+    return total % 11 == 0;
+}
+
+TEST_CASE("ISBN-10 code validation", "[validate_ISBN]")
+{
+   REQUIRE(validate_ISBN("0136091814") == true); 
+   REQUIRE(validate_ISBN("0136091812") == false); 
+   REQUIRE(validate_ISBN("007462542X") == true); 
+   REQUIRE(validate_ISBN("0112112425") == false); 
+}
+
+int main(int argc, char * argv[])
+{
+    // run tests
+    int result_ = Catch::Session().run(argc, argv);
+    // global clean-up...
+    return result_;
 }
