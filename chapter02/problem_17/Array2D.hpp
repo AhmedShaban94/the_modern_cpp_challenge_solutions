82781bbee1653e169ec5f3021032e7be0a83f347
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2021-07-30 09:38:28 +0400

    implementation of Array2D with test cases for chapter02/problem_17

--- chapter02/problem_17/Array2D.hpp
+++ chapter02/problem_17/Array2D.hpp
@@ -1,4 +1,7 @@
+#include <algorithm>
+#include <experimental/iterator>
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 template <typename T, std::size_t ROWS, std::size_t COLS>
@@ -11,7 +14,14 @@ public:
     Array2D() = default;
     ~Array2D() = default;
 
-    Array2D(const std::vector<T> &vec) : vec{vec} {}
+    Array2D(std::vector<T> vec) : vec{ROWS * COLS == vec.size() ? std::move(vec) : throw std::runtime_error{"dimension mismatch"}} {}
+
+    Array2D(const std::vector<std::vector<T>> &vec2d)
+    {
+        if (not(vec2d.size() == ROWS and vec2d.at(0).size() == COLS))
+            throw std::runtime_error{"dimension mismatch"};
+        std::for_each(vec2d.begin(), vec2d.end(), [this](const auto &row) { this->vec.insert(vec.end(), row.begin(), row.end()); });
+    }
 
     Array2D(const Array2D &) = default;
     Array2D &operator=(const Array2D &) = default;
@@ -60,12 +70,12 @@ public:
 
     void print() const
     {
-        for (std::size_t row = 0; row < ROWS; row++)
+        for (std::size_t r = 0; r < ROWS; r++)
         {
+            const std::vector<std::size_t> row(begin() + r * COLS, begin() + r * COLS + COLS);
             std::cout << "[ ";
-            for (std::size_t col = 0; col < COLS; col++)
-                std::cout << this->at(row, col) << ", ";
-            std::cout << "]";
+            std::copy(row.begin(), row.end(), std::experimental::make_ostream_joiner(std::cout, ", "));
+            std::cout << "]\n";
         }
     }
 };
--- chapter02/problem_17/tests.cpp
+++ chapter02/problem_17/tests.cpp
@@ -1,70 +1,132 @@
 #include "Array2D.hpp"
 #include "catch2/catch.hpp"
 
-TEST_CASE("testing", "[Array2D]")
+TEST_CASE("testing Array2D class", "[Array2D]")
 {
     // [1, 2, 3]
     // [4, 5, 6]
     // [7, 8 ,9]
 
-    SECTION("Testing non-const Array2D")
+    SECTION("Testing constructors")
     {
-        Array2D<std::size_t, 3, 3> arr{{1, 2, 3, 4, 5, 6, 7, 8, 9}};
+        REQUIRE_NOTHROW((Array2D<std::size_t, 3, 3>{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}));
+        REQUIRE_NOTHROW((Array2D<std::size_t, 3, 3>{{1, 2, 3, 4, 5, 6, 7, 8, 9}})); 
+    }
+
+    SECTION("Testing Array2D")
+    {
+        Array2D<std::size_t, 3, 3> arr{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}};
+        const auto [ROWS, COLS] = arr.size();
 
         SECTION("Testing at()")
         {
             SECTION("testing equality")
             {
-                REQUIRE(1 == arr.at(0, 0));
-                REQUIRE(2 == arr.at(0, 1));
-                REQUIRE(3 == arr.at(0, 2));
-                REQUIRE(4 == arr.at(1, 0));
-                REQUIRE(5 == arr.at(1, 1));
-                REQUIRE(6 == arr.at(1, 2));
-                REQUIRE(7 == arr.at(2, 0));
-                REQUIRE(8 == arr.at(2, 1));
-                REQUIRE(9 == arr.at(2, 2));
+                std::size_t value{1};
+                for (std::size_t i{0}; i < ROWS; i++)
+                {
+                    for (std::size_t j{0}; j < COLS; j++)
+                    {
+                        REQUIRE(value++ == arr.at(i, j));
+                    }
+                }
             }
 
             SECTION("testing changing values")
             {
-                arr.at(0, 0) = 11;
-                REQUIRE(11 == arr.at(0, 0));
-
-                arr.at(0, 1) = 12;
-                REQUIRE(12 == arr.at(0, 1));
-
-                arr.at(0, 2) = 13;
-                REQUIRE(13 == arr.at(0, 2));
-
-                arr.at(1, 0) = 14;
-                REQUIRE(14 == arr.at(1, 0));
-
-                arr.at(1, 1) = 15;
-                REQUIRE(15 == arr.at(1, 1));
-
-                arr.at(1, 2) = 16;
-                REQUIRE(16 == arr.at(1, 2));
-
-                arr.at(2, 0) = 17;
-                REQUIRE(17 == arr.at(2, 0));
-
-                arr.at(2, 1) = 18;
-                REQUIRE(18 == arr.at(2, 1));
-
-                arr.at(2, 2) = 19;
-                REQUIRE(19 == arr.at(2, 2));
+                std::size_t new_value{10};
+                for (std::size_t i{0}; i < ROWS; i++)
+                {
+                    for (std::size_t j{0}; j < COLS; j++)
+                    {
+                        arr.at(i, j) = new_value;
+                        REQUIRE(new_value == arr.at(i, j));
+                        new_value++;
+                    }
+                }
             }
 
             SECTION("Testing out-of-range")
             {
                 REQUIRE_THROWS_AS(arr.at(2, 3), std::out_of_range);
+                REQUIRE_THROWS_AS(arr.at(3, 3), std::out_of_range);
+                REQUIRE_THROWS_AS(arr.at(4, 3), std::out_of_range);
+                REQUIRE_THROWS_AS(arr.at(4, 4), std::out_of_range);
+                REQUIRE_THROWS_AS(arr.at(-1, -2), std::out_of_range);
             }
         }
-    }
 
-    SECTION("Testing const Array2D")
-    {
-        const Array2D<std::size_t, 3, 3> const_arr{{1, 2, 3, 4, 5, 6, 7, 8, 9}};
+        SECTION("Testing () operator")
+        {
+            SECTION("testing equality")
+            {
+                std::size_t value{1};
+                for (std::size_t i{0}; i < ROWS; i++)
+                {
+                    for (std::size_t j{0}; j < COLS; j++)
+                    {
+                        REQUIRE(value++ == arr(i, j));
+                    }
+                }
+            }
+
+            SECTION("testing changing values")
+            {
+                std::size_t new_value{10};
+                for (std::size_t i{0}; i < ROWS; i++)
+                {
+                    for (std::size_t j{0}; j < COLS; j++)
+                    {
+                        arr.at(i, j) = new_value;
+                        REQUIRE(new_value == arr(i, j));
+                        new_value++;
+                    }
+                }
+            }
+
+            SECTION("Testing out-of-range")
+            {
+                REQUIRE_THROWS_AS(arr(2, 3), std::out_of_range);
+                REQUIRE_THROWS_AS(arr(3, 3), std::out_of_range);
+                REQUIRE_THROWS_AS(arr(4, 3), std::out_of_range);
+                REQUIRE_THROWS_AS(arr(4, 4), std::out_of_range);
+                REQUIRE_THROWS_AS(arr(-1, -2), std::out_of_range);
+            }
+        }
+
+        SECTION("Testing data(), begin() & end()")
+        {
+            REQUIRE_NOTHROW(arr.data());
+            REQUIRE_NOTHROW(arr.begin());
+            REQUIRE_NOTHROW(arr.end());
+        }
+
+        SECTION("Testing fill()")
+        {
+            constexpr auto VALUE{10};
+            arr.fill(VALUE);
+            std::for_each(arr.begin(), arr.end(), [](const auto &ele) { REQUIRE(ele == VALUE); });
+        }
+
+        SECTION("Testing swap()")
+        {
+            Array2D<std::size_t, 3, 3> other{{11, 12, 13, 14, 15, 16, 17, 18, 19}};
+            arr.swap(other);
+
+            std::size_t value{11};
+            std::for_each(arr.begin(), arr.end(), [&value](const auto &ele) { REQUIRE(ele == value++); });
+        }
+
+        SECTION("Testing dimension mismatch")
+        {
+            REQUIRE_THROWS_AS((Array2D<std::size_t, 3, 3>{{1, 2, 3}}), std::runtime_error);
+            REQUIRE_THROWS_AS((Array2D<std::size_t, 1, 1>{{1, 2, 3, 4, 5, 6, 7, 8}}), std::runtime_error);
+            REQUIRE_THROWS_AS((Array2D<std::size_t, 1, 1>{{{1, 2, 3}, {4, 5, 6}, {7, 8}}}), std::runtime_error);
+        }
+
+        SECTION("Testing print()")
+        {
+            REQUIRE_NOTHROW(arr.print());
+        }
     }
 }
