c9504c454e434d8c8393cf255f35eb6ab9c83b69
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2021-08-17 20:28:36 +0400

    implmentation of chapter02/problem22

---
+++ chapter02/problem_22/main.cpp
@@ -0,0 +1,48 @@
+#include "catch2/catch.hpp"
+#include <iostream>
+
+enum class TEMP_CONVERT_UNITS
+{
+    TO_KELVIN,
+    TO_FAHRENHEIT
+};
+
+long double operator""_deg(long double temp) { return temp; }
+long double operator""_k(long double temp) { return temp - 273.15; }
+long double operator""_f(long double temp) { return (temp - 32) * 0.5556; }
+
+long double convert_temp(const long double &degree, const TEMP_CONVERT_UNITS &unit)
+{
+    if (unit == TEMP_CONVERT_UNITS::TO_KELVIN)
+        return degree + 273.15;
+    else if (unit == TEMP_CONVERT_UNITS::TO_FAHRENHEIT)
+        return (degree / 0.5556) + 32;
+    else
+        return degree;
+}
+
+TEST_CASE("temperature conversion and user-defined literals", "[temp_conversion]")
+{
+    SECTION("Testing (_deg) user-defined literal operator")
+    {
+        REQUIRE(33.0_deg == Approx(33.0).margin(1e-2));
+        REQUIRE(10.0_deg == Approx(10.0).margin(1e-2));
+        REQUIRE(33.5_deg == Approx(33.5).margin(1e-2));
+        REQUIRE(10.5_deg == Approx(10.5).margin(1e-2));
+    }
+
+    SECTION("Testing (_f) user-defined literal operator")
+    {
+        REQUIRE(33.0_f == Approx(0.5556).margin(1e-2));
+        REQUIRE(10.0_f == Approx(-12.2222).margin(1e-2));
+        REQUIRE(33.5_f == Approx(0.8333).margin(1e-2));
+        REQUIRE(10.5_f == Approx(-11.9444).margin(1e-2));
+    }
+
+    SECTION("Testing (_k) user-defined literal operator")
+    {
+        REQUIRE(33.0_k == Approx(-240.0).margin(1e-0));
+        REQUIRE(10.0_k == Approx(-263.0).margin(1e-0));
+        REQUIRE(10.5_k == Approx(-262.6).margin(1e-0));
+    }
+}
