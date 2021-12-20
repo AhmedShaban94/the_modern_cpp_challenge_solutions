4c577a212eceae25c49fbca976ab56fe75bb50b7
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2021-12-20 10:39:38 +0400

    implementation of problem_37/chapter04

---
+++ chapter04/problem_37/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <experimental/filesystem>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+namespace fs = std::experimental::filesystem;
+
+std::vector<fs::path> findFilesWithRegex(const std::string &directory, const std::string &regexp)
+{
+    const std::regex rgx{regexp};
+    std::vector<fs::path> out;
+    for (const auto &path : fs::directory_iterator{directory})
+        if (std::regex_match(path.path().filename().string(), rgx))
+            out.emplace_back(path);
+    return out;
+}
+
+int main()
+{
+    const auto out = findFilesWithRegex("files", "abc.*"); 
+    for(const auto& e : out)
+        std::cout << e.filename().string() << '\n';
+    return EXIT_SUCCESS;
+}
