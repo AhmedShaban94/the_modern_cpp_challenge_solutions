baa1160977ec8399a9d1bc20c663a34edcc7bbe9
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2022-01-06 12:10:06 +0400

    Done implementation of chapter05/problem_39

--- chapter05/problem_39/main.cpp
+++ chapter05/problem_39/main.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <iostream>
 #include <limits>
+#include <thread>
+#include <type_traits>
 
 template <typename clock_t, typename duration_t>
 class Timer
@@ -13,18 +15,27 @@ public:
     ~Timer()
     {
         const auto end = clock_t::now();
-        std::cout << std::chrono::duration_cast<duration_t>(end - start).count() << '\n';
+        std::cout << "Execution time: " << std::chrono::duration_cast<duration_t>(end - start).count();
+        if (std::is_same_v<std::chrono::seconds, duration_t>)
+            std::cout << " s.\n";
+        else if (std::is_same_v<std::chrono::milliseconds, duration_t>)
+            std::cout << " milli.\n";
+        else if (std::is_same_v<std::chrono::microseconds, duration_t>)
+            std::cout << " µs\n";
+        else if (std::is_same_v<std::chrono::nanoseconds, duration_t>)
+            std::cout << " ns\n";
     }
 };
 
 void func()
 {
+    using namespace std::chrono_literals;
     Timer<std::chrono::system_clock, std::chrono::milliseconds> t{};
-    for (std::size_t i{0}; i < INT16_MAX; ++i)
-        std::cout << i << '\n';
+    std::this_thread::sleep_for(5s);
 }
 
 int main()
 {
+    func();
     return EXIT_SUCCESS;
 }
