20beddc7ac841d257296ceea467c2064fd633ad3
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2022-01-07 22:27:36 +0400

    implementation of chapter05/problem_40

---
+++ chapter05/problem_40/main.cpp
@@ -0,0 +1,21 @@
+#include "catch2/catch.hpp"
+#include "date/date.h"
+#include <iostream>
+
+using namespace date::literals;
+using date_t = date::year_month_day;
+
+int daysBetweenTwoDates(const date_t& date_1, const date_t& date_2)
+{
+    return std::abs(
+        (date::sys_days{ date_1 } - date::sys_days{ date_2 }).count());
+}
+
+TEST_CASE("Count days between two dates", "[days_between_two_dates]")
+{
+    REQUIRE(daysBetweenTwoDates(2022_y / jan / 1, 2022_y / jan / 7) == 6);
+    REQUIRE(daysBetweenTwoDates(2022_y / jan / 7, 2022_y / jan / 1) == 6);
+    REQUIRE(daysBetweenTwoDates(2022_y / jan / 7, 2022_y / jan / 7) == 0);
+    REQUIRE(daysBetweenTwoDates(2021_y / jan / 1, 2021_y / dec / 31) == 364);
+    REQUIRE(daysBetweenTwoDates(2024_y / jan / 1, 2024_y / dec / 31) == 365); // leap year
+}
