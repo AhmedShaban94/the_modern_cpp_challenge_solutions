114e0adbfb5bd4bcc53d480b8d872906ad7868ce
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2022-01-07 19:32:50 +0400

    Updated chapter05/problem_39

--- chapter05/problem_39/main.cpp
+++ chapter05/problem_39/main.cpp
@@ -8,14 +8,28 @@ template <typename clock_t, typename duration_t>
 class Timer
 {
 private:
-    std::chrono::time_point<clock_t> start;
+    std::chrono::time_point<clock_t> m_start;
+    std::string m_functionName{};
 
 public:
-    Timer() : start{clock_t::now()} {}
+    Timer() : m_start{ clock_t::now() }
+    {
+    }
+    Timer(const std::string& functionName)
+        : m_start{ clock_t::now() }, m_functionName{ functionName }
+    {
+    }
+
     ~Timer()
     {
         const auto end = clock_t::now();
-        std::cout << "Execution time: " << std::chrono::duration_cast<duration_t>(end - start).count();
+        const auto duration
+            = std::chrono::duration_cast<duration_t>(end - m_start).count();
+        std::cout << (m_functionName.empty()
+                          ? std::string{ "Execution Time: " }
+                          : std::string{ "Execution time of function {" }
+                              + m_functionName + "}: ");
+        std::cout << duration;
         if (std::is_same_v<std::chrono::seconds, duration_t>)
             std::cout << " sec.\n";
         else if (std::is_same_v<std::chrono::milliseconds, duration_t>)
