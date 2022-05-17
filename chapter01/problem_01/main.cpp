f3a28fb862860d46c062e926b22c894d38ae63f9
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2022-05-17 23:45:35 +0200

    chapter01/problem_01 testing.

--- chapter01/problem_01/main.cpp
+++ chapter01/problem_01/main.cpp
@@ -1,21 +1,26 @@
-#include "iostream" 
+#include "catch2/catch.hpp"
+#include "iostream"
 #include <numeric>
 #include <vector>
 
-// Chapter-1 -> problem 1  (sum of divisable numbers on 3 or 5)
-int main()
+std::size_t sumOfDivisableNumbers(const std::size_t& limit)
 {
-    unsigned int limit;
-    std::vector<size_t> vec;
-
-    std::cout << "Enter the limit.\nNote: The limit is included\n";
-    std::cin >> limit;
+    std::vector<std::size_t> vec{};
 
     for (std::size_t i = 3; i <= limit; ++i)
-        if (i % 3 == 0 || i % 5 == 0)
-            vec.push_back(i);
+        if (i % 3 == 0 or i % 5 == 0)
+            vec.emplace_back(i);
 
-    std::cout << "sum: " << std::accumulate(vec.begin(), vec.end(), (size_t)0)
-              << '\n';
-    return EXIT_SUCCESS;
+    return std::accumulate(vec.begin(), vec.end(), static_cast<std::size_t>(0));
+}
+
+// Chapter-1 -> problem 1  (sum of divisable numbers on 3 or 5)
+TEST_CASE("Count days between two dates", "[days_between_two_dates]")
+{
+    REQUIRE(sumOfDivisableNumbers(10) == 33);
+    REQUIRE(sumOfDivisableNumbers(15) == 60);
+    REQUIRE(sumOfDivisableNumbers(22) == 119);
+    REQUIRE(sumOfDivisableNumbers(35) == 293);
+    REQUIRE(sumOfDivisableNumbers(50) == 593);
+    REQUIRE(sumOfDivisableNumbers(100) == 2418);
 }
