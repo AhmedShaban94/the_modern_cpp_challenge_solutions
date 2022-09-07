16ae635e6c2453e4a3258ef850123c11abcca937
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2022-09-07 12:53:26 +0400

    Updated chapter01/problem06 testing

--- chapter01/problem_06/main.cpp
+++ chapter01/problem_06/main.cpp
@@ -1,15 +1,13 @@
+#include "catch2/catch.hpp"
 #include <iostream>
 #include <numeric>
 #include <vector>
 
-int main()
+using PairsVector = std::vector<std::pair<int, int>>;
+PairsVector abundantNumbers(const std::size_t& limit)
 {
-    unsigned int limit;
-    std::cout << "Enter the limit \n";
-    std::cin >> limit;
-
-    std::vector<size_t> divisors;
-    std::vector<std::pair<int, int>> nums;
+    std::vector<std::size_t> divisors{};
+    PairsVector nums{};
     for (std::size_t i = 1; i < limit; ++i)
     {
         for (std::size_t j = 1; j < i; ++j)
@@ -18,14 +16,19 @@ int main()
                 divisors.push_back(j);
         }
         auto divisors_sum
-            = std::accumulate(divisors.begin(), divisors.end(), (size_t)0);
+            = std::accumulate(divisors.begin(), divisors.end(), (std::size_t)0);
         if (divisors_sum > i)
             nums.emplace_back(i, divisors_sum - i);
+        divisors.clear();
     }
+    return nums;
+}
 
-    std::cout << "pairs of abundant numbers and its abundance\n";
-    for (const auto& pair : nums)
-        std::cout << '(' << pair.first << ", " << pair.second << ")\n";
-
-    return EXIT_SUCCESS;
+// chapter-1 -> problem6 (abundant numbers)
+TEST_CASE("Get abundant numbers up to specific limit", "[abundant_numbers]")
+{
+    PairsVector expected{ { 12, 4 },  { 18, 3 },  { 20, 2 },  { 24, 12 },
+                          { 30, 12 }, { 36, 19 }, { 40, 10 }, { 42, 12 },
+                          { 48, 28 }, { 54, 12 } };
+    REQUIRE(abundantNumbers(55) == expected);
 }
