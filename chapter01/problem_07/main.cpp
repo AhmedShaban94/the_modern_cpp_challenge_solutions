fd6c9ffdcfa4fb2b0d337346a259385bd65999ab
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2022-09-07 13:26:08 +0400

    Update chapter01/problem07 testing

--- chapter01/problem_07/main.cpp
+++ chapter01/problem_07/main.cpp
@@ -1,65 +1,46 @@
+#include "catch2/catch.hpp"
+#include <cmath>
 #include <iostream>
 #include <set>
-#include <cmath>
 
-int sum_proper_divisors(int const number)
+using AmicablePair = std::pair<std::size_t, std::size_t>;
+
+std::size_t sum_proper_divisors(const std::size_t& number)
 {
-   int result = 1;
-   for (int i = 2; i <= std::sqrt(number); i++)
-   {
-      if (number%i == 0)
-      {
-         result += (i == (number / i)) ? i : (i + number / i);
-      }
-   }
-
-   return result;
+    std::size_t result = 1;
+    for (std::size_t i = 2; i <= std::sqrt(number); i++)
+    {
+        if (number % i == 0)
+            result += (i == (number / i)) ? i : (i + number / i);
+    }
+    return result;
 }
 
-void print_amicables(int const limit)
+std::vector<AmicablePair> getAmicableNumbers(const std::size_t& limit)
 {
-   for (int number = 4; number < limit; ++number)
-   {
-      auto sum1 = sum_proper_divisors(number);
-
-      if (sum1 < limit)
-      {
-         auto sum2 = sum_proper_divisors(sum1);
-
-         if (sum2 == number && number != sum1)
-         {
-            std::cout << number << "," << sum1 << std::endl;
-         }
-      }
-   }
+    std::vector<AmicablePair> amicablePairs{};
+    for (std::size_t number = 4; number < limit; ++number)
+    {
+        const auto sum1 = sum_proper_divisors(number);
+        if (sum1 < limit)
+        {
+            auto sum2 = sum_proper_divisors(sum1);
+            if (sum2 == number and number not_eq sum1)
+                amicablePairs.emplace_back(number, sum1);
+        }
+    }
+    return amicablePairs;
 }
 
-void print_amicables_once(int const limit)
+// chapter-1 -> problem7 (amicable numbers)
+TEST_CASE("Get amicable numbers up to specific limit", "[amicable_numbers]")
 {
-   std::set<int> printed;
-   for (int number = 4; number < limit; ++number)
-   {
-      if (printed.find(number) != printed.end()) continue;
-
-      auto sum1 = sum_proper_divisors(number);
-
-      if (sum1 < limit)
-      {
-         auto sum2 = sum_proper_divisors(sum1);
-
-         if (sum2 == number && number != sum1)
-         {
-            printed.insert(number);
-            printed.insert(sum1);
-
-            std::cout << number << "," << sum1 << std::endl;
-         }
-      }
-   }
-}
-
-int main()
-{
-   print_amicables(1000000);
-   print_amicables_once(1000000);
+    std::vector<AmicablePair> expected{
+        { 220, 284 },     { 284, 220 },     { 1184, 1210 },   { 1210, 1184 },
+        { 2620, 2924 },   { 2924, 2620 },   { 5020, 5564 },   { 5564, 5020 },
+        { 6232, 6368 },   { 6368, 6232 },   { 10744, 10856 }, { 10856, 10744 },
+        { 12285, 14595 }, { 14595, 12285 }, { 17296, 18416 }, { 18416, 17296 },
+        { 66928, 66992 }, { 66992, 66928 }
+    };
+    REQUIRE(getAmicableNumbers(70000) == expected);
 }
