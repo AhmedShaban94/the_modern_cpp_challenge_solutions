5fdcb7f81975c1fe5b2083332becee99e8f52adb
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2024-05-21 13:24:31 +0400

    add ALL_TESTS

--- chapter10/problem_79/src/main.cpp
+++ chapter10/problem_79/src/main.cpp
@@ -4,12 +4,12 @@
 #include "ZipLib/ZipArchive.h"
 
 int main() {
-// open archive file. 
+// open archive file.
 auto archive = ZipFile::Open(std::string{"data/sample79.zip"});
 std::cout << archive->GetEntriesCount() << '\n'; 
 
 
-// enumrate through the archive object. 
+// enumerate through the archive object. 
 // list the files that only matches the regex. 
 
   return EXIT_SUCCESS;
