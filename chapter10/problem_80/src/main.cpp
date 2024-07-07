2612bc96bc1034759964ffdd69bd27b232f54da2
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2024-07-07 12:59:10 +0400

    problem 80 initial code.

--- chapter10/problem_80/src/main.cpp
+++ chapter10/problem_80/src/main.cpp
@@ -9,17 +9,28 @@
 #include "ZipLib/ZipFile.h"
 #include "catch2/catch_test_macros.hpp"
 
-void compress(std::filesystem::path path_to_file)
-{
-
+void compress(std::filesystem::path destinationFile) {
+  try {
+  } catch (std::exception ex) {
+    std::cerr << ex.what() << '\n';
+  }
 }
 
-void compress() 
-{
-
+void compress(std::filesystem::path destinationDirectory) {
+  try {
+  } catch (std::exception ex) {
+    std::cerr << ex.what() << '\n';
+  }
 }
 
-// chapter-10 -> problem79 (Finding files in a ZIP archive)
-TEST_CASE("Finding files in zip file with specific pattern", "[find_files_in_zip]") {
-
+void decompress(std::filesystem::path destinationDirectory) {
+  try {
+  } catch (std::exception ex) {
+    std::cerr << ex.what() << '\n';
+  }
 }
+
+// chapter-10 -> problem80 (compress/decompress files to zip)
+TEST_CASE("Compressing file to zip", "[compress_file_to_zip]") {}
+TEST_CASE("Compressing directory to zip", "[compress_directory_to_zip]") {}
+TEST_CASE("Decompressing zip to directory", "[decompress_zip_to_directory]") {}
