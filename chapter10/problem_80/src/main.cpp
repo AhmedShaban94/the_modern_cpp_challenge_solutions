b3fac1a6ca4efd7eeba74daee727a2b602b4c658
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2024-07-07 12:10:44 +0400

    Directory refactor.

---
+++ chapter10/problem_80/src/main.cpp
@@ -0,0 +1,25 @@
+#include <exception>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "ZipLib/ZipArchive.h"
+#include "ZipLib/ZipFile.h"
+#include "catch2/catch_test_macros.hpp"
+
+void compress(std::filesystem::path path_to_file)
+{
+
+}
+
+void compress() 
+{
+
+}
+
+// chapter-10 -> problem79 (Finding files in a ZIP archive)
+TEST_CASE("Finding files in zip file with specific pattern", "[find_files_in_zip]") {
+
+}
