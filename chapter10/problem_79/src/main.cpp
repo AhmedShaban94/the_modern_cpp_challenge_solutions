0e02bd11ad99ee0f0c5f00f3831dbc93fa652901
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2024-09-01 11:46:28 +0400

    finding files in zip file.

--- chapter10/problem_79/src/main.cpp
+++ chapter10/problem_79/src/main.cpp
@@ -8,17 +8,21 @@
 
 #include "SimZip.h"
 #include "catch2/catch_test_macros.hpp"
+#include "catch2/matchers/catch_matchers_vector.hpp"
 
 std::vector<std::string> FindFilesInZip(std::string_view regexPattern) {
   std::vector<std::string> fileNames{};
   std::regex pattern(regexPattern.data());
+
+  std::cout << "Directory: " << std::filesystem::current_path() << '\n';
+
   try {
     const std::string zipFilePath = "sample79.zip";
     const std::string outputDir = "zip_output";
     SimZip zip(zipFilePath, SimZip::OpenMode::Read);
 
     zip.extractall(outputDir);
-    for (const auto& entry : std::filesystem::directory_iterator("zip_output")) {
+    for (const auto& entry : std::filesystem::recursive_directory_iterator("zip_output")) {
       const std::string fileName = entry.path().filename().string();
       if (entry.is_regular_file() && std::regex_search(fileName, pattern)) {
         fileNames.push_back(fileName);
@@ -38,7 +42,7 @@ TEST_CASE("Finding files in zip file with specific pattern", "[find_files_in_zip
   std::vector<std::string> expectedFileNames = {"albert einstein.jpg", "einstein_nobel.jpg",
                                                 "Stephen_Hawking.StarChild.jpg", "Isaac_Newton.jpg"};
 
-  REQUIRE(FindFilesInZip(expectedRegexPattern) == expectedFileNames);
-  REQUIRE(FindFilesInZip(faultyRegexPattern).empty());
-  REQUIRE(FindFilesInZip(faultyFileName).empty());
+  CHECK_THAT(FindFilesInZip(expectedRegexPattern), Catch::Matchers::UnorderedEquals(expectedFileNames));
+  CHECK(FindFilesInZip(faultyRegexPattern).empty());
+  CHECK(FindFilesInZip(faultyFileName).empty());
 }
