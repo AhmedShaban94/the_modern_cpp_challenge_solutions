b35cf77c7fe90b828523c43565cea7ed6f0ffecf
Ahmed_Shaban <ahmed.shaban.sh@gmail.com>
2026-09-06 07:19:34 +0400

    fix: add missing <iterator> header includes in chapter03 problems 24 and 27

--- chapter03/problem_24/main.cpp
+++ chapter03/problem_24/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cctype>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 
--- chapter03/problem_27/main.cpp
+++ chapter03/problem_27/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <sstream>
 #include <string>
 #include <vector>
